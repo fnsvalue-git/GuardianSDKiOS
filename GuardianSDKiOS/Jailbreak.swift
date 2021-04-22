c18b372390b558b90a662613565fbcb44fec0e14
PunreachFNSV <punreachrany@fnsvalue.co.kr>
2021-04-22 16:27:25 +0900

    Jailbreak 추가 - info.plist에서 Cydia 확인 허락을 요청해야 합니다

---
+++ GuardianSDKiOS/Jailbreak.swift
@@ -0,0 +1,27 @@
+//
+//  JailbreakCheck.swift
+//  GuardianSDKiOS
+//
+//  Created by elite on 2021/04/22.
+//  Copyright © 2021 fns_mac_pro. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+public class Jailbreak {
+    
+    public static let sharedInstance = Jailbreak()
+    public init() {
+    }
+    
+    public func isJailBroken() -> Bool {
+        return UIDevice.current.isJailBroken
+    }
+    
+    public func isSimulator() -> Bool {
+        return UIDevice.current.isSimulator
+    }
+}
+
+
